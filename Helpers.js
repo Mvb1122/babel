190be92bdc195cc2dbcb288cad2dea83ae62dad3
Mvb1122 <micahbushman.school@gmail.com>
2025-03-27 12:46:49 -0600

    Performance update volume function

--- Helpers.js
+++ Helpers.js
@@ -29,47 +29,55 @@ export function postJSON(URL, data, stringify = true, type = 'application/json')
     })
 }
 
+let scriptProcessor = null, stream = null;
+// Extract creating audio stream out, to persist it over time.
+async function setupVolumeTrackerStream() {
+    try {
+        stream = navigator.mediaDevices.getUserMedia({
+            audio: true,
+            video: false
+        });
+        const audioContext = new AudioContext();
+        const analyser = audioContext.createAnalyser();
+        const microphone = audioContext.createMediaStreamSource(await stream);
+        scriptProcessor = audioContext.createScriptProcessor(16384, 1, 1);
+
+        analyser.smoothingTimeConstant = 0.8;
+        analyser.fftSize = 1024;
+
+        microphone.connect(analyser);
+        analyser.connect(scriptProcessor);
+        scriptProcessor.connect(audioContext.destination);
+
+        return; // Now scriptProcessor is all setup.
+    } catch (err) {
+        console.error(err);
+        throw err;
+    }
+}
+
+async function stopVolumeTrackerStream() {
+    (await stream).getTracks().forEach(track => {
+        track.stop();
+    });
+}
+
 /** Tasty stolen code :3 
  * @returns {Promise<number>} 0-100 representing volume.
  * @see https://stackoverflow.com/questions/33322681/checking-microphone-volume-in-javascript
 */
 export function GetAverageVolume() {
-    return new Promise((resolve, reject) => {
-        navigator.mediaDevices.getUserMedia({
-            audio: true,
-            video: false
-        })
-            .then(function(stream) {
-                const audioContext = new AudioContext();
-                const analyser = audioContext.createAnalyser();
-                const microphone = audioContext.createMediaStreamSource(stream);
-                const scriptProcessor = audioContext.createScriptProcessor(16384, 1, 1);
-            
-                analyser.smoothingTimeConstant = 0.8;
-                analyser.fftSize = 1024;
-            
-                microphone.connect(analyser);
-                analyser.connect(scriptProcessor);
-                scriptProcessor.connect(audioContext.destination);
-                scriptProcessor.onaudioprocess = function() {
-                    const array = new Uint8Array(analyser.frequencyBinCount);
-                    analyser.getByteFrequencyData(array);
-                    const arraySum = array.reduce((a, value) => a + value, 0);
-                    const average = arraySum / array.length;
-                    
-                    // Stop tracking because we got our number.
-                    stream.getTracks().forEach(track => {
-                        track.stop();
-                    });
+    return new Promise(async (resolve, reject) => {
+        if (scriptProcessor == null) await setupVolumeTrackerStream();
 
-                    resolve(Math.round(average));
-                };
-            })
-            .catch(function(err) {
-                /* handle the error */
-                console.error(err);
-                reject(err);
-            });
+        scriptProcessor.onaudioprocess = function() {
+            const array = new Uint8Array(analyser.frequencyBinCount);
+            analyser.getByteFrequencyData(array);
+            const arraySum = array.reduce((a, value) => a + value, 0);
+            const average = arraySum / array.length;
+
+            resolve(Math.round(average));
+        };
     })
 }
 
