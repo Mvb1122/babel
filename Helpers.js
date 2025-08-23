ad92672436cac1256226b08f3b7fdcb0b89680dc
Mvb1122 <micahbushman.school@gmail.com>
2025-08-23 12:35:09 -0600

    Add my favicon bc the error was bugging me.

--- Helpers.js
+++ Helpers.js
@@ -29,63 +29,6 @@ export function postJSON(URL, data, stringify = true, type = 'application/json')
     })
 }
 
-let scriptProcessor = null, stream = null, analyser = null;
-// Extract creating audio stream out, to persist it over time.
-async function setupVolumeTrackerStream() {
-    try {
-        stream = navigator.mediaDevices.getUserMedia({
-            audio: true,
-            video: false
-        });
-        const audioContext = new AudioContext();
-        analyser = audioContext.createAnalyser();
-        const microphone = audioContext.createMediaStreamSource(await stream);
-        scriptProcessor = audioContext.createScriptProcessor(16384, 1, 1);
-
-        analyser.smoothingTimeConstant = 0.8;
-        analyser.fftSize = 1024;
-
-        microphone.connect(analyser);
-        analyser.connect(scriptProcessor);
-        scriptProcessor.connect(audioContext.destination);
-
-        return; // Now scriptProcessor is all setup.
-    } catch (err) {
-        console.error(err);
-        throw err;
-    }
-}
-
-async function stopVolumeTrackerStream() {
-    (await stream).getTracks().forEach(track => {
-        track.stop();
-    });
-}
-
-/** Tasty stolen code :3 
- * @returns {Promise<number>} 0-100 representing volume.
- * @see https://stackoverflow.com/questions/33322681/checking-microphone-volume-in-javascript
-*/
-export function GetAverageVolume() {
-    return new Promise(async (resolve, reject) => {
-        // if (scriptProcessor == null) await setupVolumeTrackerStream();
-
-        if (scriptProcessor != null)
-            scriptProcessor.onaudioprocess = function() {
-                const array = new Uint8Array(analyser.frequencyBinCount);
-                analyser.getByteFrequencyData(array);
-                const arraySum = array.reduce((a, value) => a + value, 0);
-                const average = arraySum / array.length;
-
-                // Remove this listener after we're done. 
-                scriptProcessor.onaudioprocess = null;
-
-                resolve(Math.round(average));
-            };
-        else resolve(0);
-    })
-}
-
 export function blobToBase64(blob) {
     return new Promise((resolve, _) => {
         const reader = new FileReader();
